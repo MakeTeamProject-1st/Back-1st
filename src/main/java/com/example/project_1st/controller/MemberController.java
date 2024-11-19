efc62861e6fec7980573325cdf66ea1a311de033
Wayne <wayne02990@gmail.com>
2024-11-19 23:33:36 +0900

    회원가입 작업중입니다.

--- src/main/java/com/example/project_1st/controller/MemberController.java
+++ src/main/java/com/example/project_1st/controller/MemberController.java
@@ -14,13 +14,13 @@ public class MemberController {
 
     private final MemberService memberService;  // 생성자 주입하기.(서비스계층 의존성 주입)
 
-    // TODO - 회원가입 페이지 요청 처리하기.
+    //  회원가입 페이지 요청 처리하기.
     @GetMapping("/member/save")
     public String saveForm() {
         return "save";  // 회원가입 페이지 출력하기.
     }
 
-    // TODO - 회원가입 요청 처리하기.
+    //  회원가입 요청 처리하기.
     public String save(ModelAttribute MemberDTO memberDTO) {
         // 정상적인 행위가 되는지 확인. (디버그용)
         System.out.println("MemberController.save");
@@ -29,7 +29,7 @@ public class MemberController {
         return "login";     // 회원가입 성공 후 로그인 페이지로 이동.
     }
 
-    // TODO - 로그인 요청 처리하기.
+    //  로그인 요청 처리하기.
     public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
         MemberDTO loginResult = memberService.login(memberDTO);     // 서비스 계층에서 로그인 처리 반환하기.
 
--- src/main/java/com/example/project_1st/service/MemberService.java
+++ src/main/java/com/example/project_1st/service/MemberService.java
@@ -5,14 +5,14 @@ import com.example.project_1st.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-// TODO 회원 비즈니스 로직 처리하는 서비스 클래스 만들기.
+// 회원 비즈니스 로직 처리하는 서비스 클래스 만들기.
 @Service    // Spring Bean 으로 등록해서, 의존성주입 가능.
 @RequiredArgsConstructor    // final field 에 대해 생성자 자동생성 ( LOMBOK )
 public class MemberService {
 
     private final MemberRepository memberRepository;    // 회원정보 관리위한 JPA 레포 의존성 주입하기.
 
-    // TODO 회원가입 로직
+    // 회원가입 로직
     public void save(MemberDTO memberDTO){
 
     }
